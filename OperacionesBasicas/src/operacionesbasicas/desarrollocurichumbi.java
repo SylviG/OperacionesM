8cee8f0056f2bfda49691358907685cc311dc2c6
PC 13 <PC 13@172.21.106.97>
2015-11-10 09:00:44 -0500

    sdfsfs

--- OperacionesBasicas/src/operacionesbasicas/desarrollocurichumbi.java
+++ OperacionesBasicas/src/operacionesbasicas/desarrollocurichumbi.java
@@ -14,6 +14,7 @@ public class desarrollocurichumbi {
     public void oscar(){
         System.out.println("hola como estan ");
         System.out.println("Ramos bien ");
+        //Bien Panata
     }
     
 }
